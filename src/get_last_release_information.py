c3dead1c2599519ed0b06d263e4e9f62a6213e22
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2024-06-24 21:32:41 +0200

    -

--- src/get_last_release_information.py
+++ src/get_last_release_information.py
@@ -1,8 +1,12 @@
+import json
 from os import getenv
+from sys import stderr
 import requests
 
 if __name__ == "__main__":
     token = getenv("GITHUB_TOKEN")
     r = requests.get('https://api.github.com/repos/JohannesK71083/actions_test/releases', headers={'Accept': 'application/vnd.github+json', 'Authorization': f"Bearer {token}"})
     js = r.json()[0]
+    print(js["body"], file=stderr)
+    print(json.dumps(js), file=stderr)
     print(f'{js["html_url"]}|{js["tag_name"]}|{js["body"]}')
