8a2637fab07a335bfebb7cad3696405dda57fcef
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2024-06-24 16:41:38 +0200

    -

--- src/validate_inputs.py
+++ src/validate_inputs.py
@@ -1,5 +1,7 @@
 from os import getenv
 
 if __name__ == "__main__":
-    print(getenv("prerelease"), type(getenv("prerelease")))
-    print(getenv("mode"))
+    if not (v := getenv("prerelease")) in ["true", "false"]:
+        raise ValueError(f"input 'prerelease' has an invalid value: {v}")
+    if not (v := getenv("mode")) in ["major", "minor", "pre"]:
+        raise ValueError(f"input 'mode' has an invalid value: {v}")
