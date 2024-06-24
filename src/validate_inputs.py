f6ef6f21ca9872c58fac5eb0c6fd678740fa9dfd
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2024-06-24 16:44:56 +0200

    -

--- src/validate_inputs.py
+++ src/validate_inputs.py
@@ -1,7 +1,12 @@
 from os import getenv
+from typing import Any
+
+class InputError(ValueError):
+    def __init__(self, input_name: str, value: Any) -> None:
+        super().__init__(f"input '{input_name}' has the invalid value '{value}'")
 
 if __name__ == "__main__":
     if not (v := getenv("prerelease")) in ["true", "false"]:
-        raise ValueError(f"input 'prerelease' has an invalid value: {v}")
+        raise InputError("prerelease", v)
     if not (v := getenv("mode")) in ["major", "minor", "pre"]:
-        raise ValueError(f"input 'mode' has an invalid value: {v}")
+        raise InputError("mode", v)
