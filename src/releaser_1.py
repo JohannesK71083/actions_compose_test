25524c1fbb0899312e3c7556089cb590d8fe9f4a
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2024-07-19 18:05:24 +0200

    -

--- src/releaser_1.py
+++ src/releaser_1.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from os import path
+from os import path, system
 import re
 from sys import exc_info, stderr
 from traceback import format_exc
@@ -333,6 +333,9 @@ def generate_new_release_information(version: Version, tag_components: tuple[tup
     if version_text_repo_file_path != None:
         with open(version_text_repo_file_path, "w") as f:
             f.write(new_version_text.replace("{Maj}", str(new_version[0])).replace("{Min}", str(new_version[1])).replace("{Pre}", str(new_version[2])))
+        system(f"git -C {ENVStorage.WORK_PATH}/checkout add {version_text_repo_file_path}")
+        system(f"git -C {ENVStorage.WORK_PATH}/checkout commit -m 'Update version text file'")  # NOTE
+        system(f"git -C {ENVStorage.WORK_PATH}/checkout push")
 
     OutputStorage.tag = new_tag
     OutputStorage.title = new_title
