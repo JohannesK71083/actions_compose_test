3e9510a0c5a22b272ef8931b638c41a1544f523e
Johannes Kerger <49689041+JohannesK71083@users.noreply.github.com>
2024-07-19 18:15:41 +0200

    -

--- src/releaser_1.py
+++ src/releaser_1.py
@@ -339,6 +339,8 @@ def generate_new_release_information(version: Version, tag_components: tuple[tup
     if version_text_repo_file_path != None:
         with open(version_text_repo_file_path, "w") as f:
             f.write(new_version_text.replace("{Maj}", str(new_version[0])).replace("{Min}", str(new_version[1])).replace("{Pre}", str(new_version[2])))
+        command("git config --global user.name 'github-actions[bot]'")
+        command("git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'")
         command(f"git -C {ENVStorage.WORK_PATH}/checkout add {version_text_repo_file_path}")
         command(f"git -C {ENVStorage.WORK_PATH}/checkout commit -m 'Update version text file'")  # NOTE
         command(f"git -C {ENVStorage.WORK_PATH}/checkout push")
